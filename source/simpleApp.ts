0ef4d2e080800ade57b9c0b68130e11aa33bbdd7
Judah Holanda Correia Lima <judahholanda7@gmail.com>
2022-06-21 08:48:37 -0300

    extends routersingleton

--- source/simpleApp.ts
+++ source/simpleApp.ts
@@ -1,38 +1,17 @@
 import express from 'express';
 
 import RouterSingleton from './router/routerSingleton';
-import { DatabaseHandler, IDatabaseHandler } from 'backapi';
-import execute from './loader';
+import { DatabaseHandler } from 'backapi';
+import { SimpleApp as SA } from 'backapirest';
 
 import dotEnv from 'dotenv';
 
 dotEnv.config();
 
-export default class SimpleApp {
+export default class SimpleApp extends SA {
   express: express.Application;
-  router: RouterSingleton;
-  databaseHandler: DatabaseHandler;
+
   constructor(router: RouterSingleton, databaseHandler: DatabaseHandler) {
-    this.express = express();
-    this.middlewares();
-    this.router = router;
-    this.databaseHandler = databaseHandler;
-    this.routes(databaseHandler.getInit());
-  }
-
-  protected middlewares(): void {
-    this.express.use(express.json());
-  }
-
-  protected async routes(initDefault?: IDatabaseHandler): Promise<void> {
-    const port = process.env.PORT || 3000;
-
-    this.router.createRoutes(initDefault);
-    await execute(this.router);
-    await this.express.use(this.router.getRoutes());
-    await this.express.listen(port);
-    console.log(
-      `started server on 0.0.0.0:${port}, url: http://localhost:${port}`
-    );
+    super(router, databaseHandler, express);
   }
 }
