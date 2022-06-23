c2a21c2e86437af3de0346c21d923d7d550cce9d
Restyled.io <commits@restyled.io>
2022-06-23 17:42:02 +0000

    Restyled by shellharden

--- script/back.sh
+++ script/back.sh
@@ -11,32 +11,32 @@ dist="${npm_package_config_path_dist:-.}"
 server="${npm_package_config_path_server:-source/server.js}"
 file=$dist/$server
 
-pwd=$(pwd)
+pwd=$PWD
 
-cd $pwd;
+cd "$pwd";
 
 if [ -f ".env" ]; then
     echo ".env exists."
     cat .env | grep -v '#' | grep PORT
-    export $(cat .env | grep -v '#' | grep PORT)
+    export "$(cat .env | grep -v '#' | grep PORT)"
 fi
 
 case $exec in
   "dev")
     echo "Starting dev server"
-    (cd $pwd ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
+    (cd "$pwd" ; ./node_modules/nodemon/bin/nodemon.js -e ts --exec "npm run build && npm run start")
     ;;
 
   "build")
     echo "Starting build"
-    (cd $pwd ; npm run --prefix $pwd tsc)
+    (cd "$pwd" ; npm run --prefix "$pwd" tsc)
     ;;
 
   "")
     echo "Starting"
     if test -f "$file"; then
-      (node $file)
+      (node "$file")
     else
-      (node ./node_modules/@backapirest/express/script/simpleServer.mjs -f $pwd)
+      (node ./node_modules/@backapirest/express/script/simpleServer.mjs -f "$pwd")
     fi;;
 esac
